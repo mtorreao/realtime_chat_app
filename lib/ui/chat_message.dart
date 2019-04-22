ec3335aca1f43829caaebcb6598a32ee41f884ef
Matheus Torreão <matheus.torreao@avanade.com>
2019-04-22 19:34:43 -0300

    Novo widget stateless ChatMessagem

---
+++ lib/ui/chat_message.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+
+  final Map<String, dynamic> data;
+
+  ChatMessage(this.data);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(data["senderPhotoUrl"]),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  data["senderName"],
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                    margin: const EdgeInsets.only(top: 5.0),
+                    child: data["imgUrl"] != null ?
+                    Image.network(data["imgUrl"], width: 250.0,) :
+                    Text(data["text"])
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
