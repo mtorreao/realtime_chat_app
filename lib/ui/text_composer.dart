01e506c6f4b09d12320373bf8a1db5fa0d4d9c7b
Matheus Torreão <matheus.torreao@avanade.com>
2019-04-29 13:52:35 -0300

    Funcionalidade: Envio de Imagens

--- lib/ui/text_composer.dart
+++ lib/ui/text_composer.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:realtime_chat_app/util/auth_service.dart';
 import 'package:realtime_chat_app/util/message.dart';
 
 class TextComposer extends StatefulWidget {
-
   TextComposer();
 
   @override
@@ -21,11 +22,30 @@ class _TextComposerState extends State<TextComposer> {
       child: Container(
           child: Row(
             children: <Widget>[
-              IconButton(icon: Icon(Icons.photo_camera), onPressed: null),
+              IconButton(
+                  icon: Icon(Icons.photo_camera),
+                  onPressed: () async {
+                    var user = await AuthService().handleSignIn();
+                    var file =
+                    await ImagePicker.pickImage(source: ImageSource.camera);
+                    if (file == null) return;
+                    StorageUploadTask task = FirebaseStorage.instance
+                        .ref()
+                        .child(user.uid.toString() +
+                        DateTime
+                            .now()
+                            .millisecondsSinceEpoch
+                            .toString())
+                        .putFile(file);
+                    var snapshot = await task.onComplete;
+                    String imgUrl = await snapshot.ref.getDownloadURL();
+                    _sendMessage(imageUrl: imgUrl);
+                  }),
               Expanded(
                   child: TextField(
                     controller: _messageTextController,
-                    decoration: InputDecoration(labelText: 'Enviar uma mensagem...'),
+                    decoration: InputDecoration(
+                        labelText: 'Enviar uma mensagem...'),
                   )),
               IconButton(icon: Icon(Icons.send), onPressed: _sendMessage),
             ],
@@ -33,17 +53,19 @@ class _TextComposerState extends State<TextComposer> {
     );
   }
 
-  Future _sendMessage() async {
+  Future _sendMessage({String imageUrl}) async {
     var messageText = _messageTextController.text;
 
     var user = await AuthService().handleSignIn();
-    if (messageText.isNotEmpty) {
+    if (messageText.isNotEmpty || imageUrl.isNotEmpty) {
       try {
-        await Firestore.instance
-            .collection('realtime_chat_app_messages')
-            .add(Message(senderName: user.displayName,
-            text: messageText,
-            senderPhotoUrl: user.photoUrl).toMap());
+        await Firestore.instance.collection('realtime_chat_app_messages').add(
+            Message(
+                senderName: user.displayName,
+                text: messageText ?? null,
+                imgUrl: imageUrl ?? null,
+                senderPhotoUrl: user.photoUrl)
+                .toMap());
         Scaffold.of(context).showSnackBar(SnackBar(
           content: Text('Mensagem enviada!'),
           duration: Duration(seconds: 2),
@@ -58,5 +80,3 @@ class _TextComposerState extends State<TextComposer> {
     }
   }
 }
-
-
